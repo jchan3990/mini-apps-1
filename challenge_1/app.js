8f3cb9ff7f4d6761ab253af696e3e5747da151ed
jchan3990 <jonchan3990@gmail.com>
2020-06-08 16:12:55 -0700

    Change Board Values to X and O alternating

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,5 +1,20 @@
 console.log('Hello!');
 
-// $('.box').click(function(event) {
-//   alert('Click!');
-// });
+var currentPlayer = 'X';
+
+var move = e => {
+  // e.preventDefault();
+  if (currentPlayer === 'X') {
+    e.target.innerHTML  = 'X';
+    currentPlayer = 'O';
+  } else {
+    e.target.innerHTML = 'O';
+    currentPlayer = 'X';
+  }
+};
+
+let box = document.getElementsByClassName("box");
+for (var i = 0; i < box.length; i++) {
+  box[i].addEventListener('click', move);
+}
+
