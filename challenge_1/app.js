d00996379b8077314b1f15504afdfeb8066b10b5
jchan3990 <jonchan3990@gmail.com>
2020-06-08 17:39:24 -0700

    Pushed into array for win checking

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,20 +1,36 @@
 console.log('Hello!');
-
 var currentPlayer = 'X';
+var playArray = [];
 
-var move = e => {
-  // e.preventDefault();
-  if (currentPlayer === 'X') {
-    e.target.innerHTML  = 'X';
-    currentPlayer = 'O';
-  } else {
-    e.target.innerHTML = 'O';
-    currentPlayer = 'X';
-  }
-};
-
+// Set box as the full collection of boxes
 let box = document.getElementsByClassName("box");
-for (var i = 0; i < box.length; i++) {
-  box[i].addEventListener('click', move);
+for (let i = 0; i < box.length; i++) {
+  playArray.push('-');
 }
 
+var move = e => {
+  if (currentPlayer === 'X') {
+    e.target.innerHTML  = 'X';
+    e.target.removeEventListener('click', move);
+    currentPlayer = 'O';
+    for (let i = 0; i < playArray.length; i++) {
+      if (box[i].innerHTML !== playArray[i]) {
+        playArray[i] = box[i].innerHTML;
+      }
+    }
+  } else {
+    e.target.innerHTML = 'O';
+    e.target.removeEventListener('click', move);
+    currentPlayer = 'X';
+    for (let i = 0; i < playArray.length; i++) {
+      if (box[i].innerHTML !== playArray[i]) {
+        playArray[i] = box[i].innerHTML;
+      }
+    }
+  }
+  console.log(playArray);
+};
+
+for (let i = 0; i < box.length; i++) {
+  box[i].addEventListener('click', move);
+}
